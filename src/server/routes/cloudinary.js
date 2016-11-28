668c881ee3600d6f09448cf3c559d23bdbdb8ff5
Adam Franzen <adamfranzen@gmail.com>
2016-11-28 15:35:50 -0700

    node.js implmentation of cloudinary working with env variables

--- src/server/routes/cloudinary.js
+++ src/server/routes/cloudinary.js
@@ -2,5 +2,5 @@ const axios = require('axios');
 
 exports.getPictures = function() {
   console.log('cloudinary.getPictures is hit');
-  return axios.get('https://447252371969227:V-FVFFtZ3P6QOxWwudo3caL1998@api.cloudinary.com/v1_1/apfranzen/resources/image')
+  return axios.get('https://447252371969227:V-FVFFtZ3P6QOxWwudo3caL1998@api.cloudinary.com/v1_1/apfranzen/resources/image');
 };
--- src/server/routes/pictures.js
+++ src/server/routes/pictures.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const cloudinary = require('./cloudinary.js');
+// const cloudinary = require('./cloudinary.js');
 const knex = require('../db/knex');
 const axios = require('axios');
+var cloudinary = require('cloudinary')
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret
+});
+
 /* get picures from db */
 // router.get('/', (req, res, next) => {
 //   return knex('pictures').select('*')
@@ -21,14 +29,25 @@ const axios = require('axios');
 /* get picures from cloudinary */
 router.get('/', (req, res, next) => {
   console.log('get pictures hit');
-  axios.get('https://447252371969227:V-FVFFtZ3P6QOxWwudo3caL1998@api.cloudinary.com/v1_1/apfranzen/resources/image')
-  .then(function (response) {
-    console.log(response.data.resources);
-    res.json(response.data.resources);
-  })
-  .catch(function (error) {
-    console.log(error);
+  // cloudinary.api.resources_by_tag("mytag", function(result){});
+  // var pictures = cloudinary.url("gsqcphwyzxruysovuh0v.jpg");
+
+  cloudinary.api.resources_by_tag("1001", function(result){
+    console.log(result.resources);
+    res.json(result.resources)
   });
+
+
+
+  // cloudinary.api.resources_by_tag("mytag", function(result){});
+  // axios.get('https://447252371969227:V-FVFFtZ3P6QOxWwudo3caL1998@api.cloudinary.com/v1_1/apfranzen/resources/image/tags/1001')
+  // .then(function (response) {
+  //   console.log(response.data.resources);
+  //   res.json(response.data.resources);
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
 });
 
 module.exports = router;
