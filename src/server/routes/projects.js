55a422509eb2dee44a99da4b19c8a3b23a572647
Adam Franzen <adamfranzen@gmail.com>
2016-12-05 10:48:09 -0700

    inbdound route created

--- src/server/routes/projects.js
+++ src/server/routes/projects.js
@@ -25,64 +25,6 @@ router.get('/', (req, res, next) => {
     console.log('err: ', err);
     return next(err); });
 });
-// router.get('/', (req, res, next) => {
-//   knex('projects').select('*')
-//   .then((projects) => {
-//     // console.log(projects[0].project);
-//     let pictures = projects.map((project) => {
-//       // console.log(project.project);
-//         return cloudinary.api.resources_by_tag(project.project, function(result){
-//           // console.log(result.resources[0].public_id);
-//         });
-//       })
-//
-//     Promise.all(pictures)
-//     .then((response) => {
-//       // console.log(response[0]);
-//       // let pictures = response.map((array) => {
-//       //   return array.resources[0].public_id;
-//       // })
-//       // return pictures;
-//     })
-//
-//     res.status(200).json({
-//       status: 'success',
-//       data: projects, pictures
-//     });
-//   })
-//   .catch((err) => { return next(err); });
-// });
-
-// /* get picures from cloudinary */
-// router.get('/', (req, res, next) => {
-//   knex('projects').select('*')
-//   .then((projects) => {
-//     return Promise.all(projects.map(project => {
-//       return requestToCloudinary(project.project).then(cloudinaryResponse => {
-//         project.image = cloudinaryResponse.resources[0]
-//         return project
-//       })
-//     }))
-//
-//     // [Promise, Promise, Promise]
-//
-//     // res.status(200).json({
-//     //   status: 'success',
-//     //   data: projects
-//     // });
-//   }).then((usersAndProjects) => {})
-//   .catch((err) => { return next(err); });
-// });
-
-// router.get('/:project', (req, res, next) => {
-//   console.log('server hit');
-//   let project = req.params.project;
-//   console.log(project);
-//   cloudinary.api.resources_by_tag(project, function(result){
-//     console.log(result);
-//     res.send(result)
-//   });
-// })
 
 /* get picures from db */
 router.get('/:project', (req, res, next) => {
